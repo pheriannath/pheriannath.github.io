7cb2dbefe85982f2d1f85091fcce1fa9534a8a60
Mike Katsufrakis <pheriannath@gmail.com>
2023-01-30 10:47:49 -0800

    block rendering if the app isn't focused

--- index.js
+++ index.js
@@ -20,41 +20,43 @@ const _contours = contours().size([x, y]);
 // Generates a new set of perlin noise and contours.
 // Inspiration from https://observablehq.com/@d3/cloud-contours
 function generate(timestamp) {
-  // Clear the canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // Don't render if the tab/browser isn't focused
+  if (document.hasFocus()) {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Set up an array to hold the noise data, then populate it
-  let values = new Float64Array(x * y);
+    // Set up an array to hold the noise data, then populate it
+    let values = new Float64Array(x * y);
 
-  for (let j = 0, k = 0; j < y; ++j) {
-    for (let i = 0; i < x; ++i, ++k) {
-      // This is largely just playing with values until it looks good
-      // noise.perlin3 is from the IIFE in perlin.js
-      values[k] = Math.abs(
-        noise.perlin3(j / 3.05, i / 3.05, timestamp / 10000) * -3
-      );
+    for (let j = 0, k = 0; j < y; ++j) {
+      for (let i = 0; i < x; ++i, ++k) {
+        // This is largely just playing with values until it looks good
+        // noise.perlin3 is from the IIFE in perlin.js
+        values[k] = Math.abs(
+          noise.perlin3(j / 3.05, i / 3.05, timestamp / 10000) * -3
+        );
+      }
+    }
+
+    // Smooths the data out
+    values = blur2({ data: values, width: x }, 3).data;
+
+    // Set up rendering
+    const context = ctx;
+    const projection = geoIdentity().scale(8);
+    const path = geoPath(projection, context);
+    context.lineWidth = 2;
+    context.lineJoin = "round";
+    context.strokeStyle = "rgb(138, 71, 206)";
+
+    const tmax = 96; // Performance is directly tied to this value. Adjust with care.
+
+    for (let tmin = 0; tmin < tmax; tmin = tmin + 8) {
+      context.beginPath();
+      path(_contours.contour(values, tmin / tmax));
+      context.stroke();
     }
   }
-
-  // Smooths the data out
-  values = blur2({ data: values, width: x }, 3).data;
-
-  // Set up rendering
-  const context = ctx;
-  const projection = geoIdentity().scale(8);
-  const path = geoPath(projection, context);
-  context.lineWidth = 2;
-  context.lineJoin = "round";
-  context.strokeStyle = "rgb(138, 71, 206)";
-
-  const tmax = 96; // Performance is directly tied to this value. Adjust with care.
-
-  for (let tmin = 0; tmin < tmax; tmin = tmin + 8) {
-    context.beginPath();
-    path(_contours.contour(values, tmin / tmax));
-    context.stroke();
-  }
-
   // Do it all over again.
   // TODO: Frame skipping, maybe.
   window.requestAnimationFrame(generate);
